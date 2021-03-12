5056c3a5e8768b1f5034a108cf9be4f639e72e27
raiguard <caleb.heuer@gmail.com>
2021-03-11 21:47:57 -0700

    Remove unused script inventory

--- src/scripts/player-data.lua
+++ src/scripts/player-data.lua
@@ -12,7 +12,6 @@ function player_data.init(player_index)
       deconstruction_planner_buttons_shown = false
     },
     guis = {},
-    holder_inventory = game.create_inventory(1),
     settings = {}
   }
 end
