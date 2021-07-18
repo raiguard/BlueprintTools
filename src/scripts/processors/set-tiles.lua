4b96ed85ec069b1c85d3e88886df16f9f974958c
raiguard <caleb.heuer@gmail.com>
2021-07-17 18:28:23 -0600

    Make set tiles work with tiles as well

--- src/scripts/processors/set-tiles.lua
+++ src/scripts/processors/set-tiles.lua
@@ -7,14 +7,17 @@ return function(player, tile_name, fill_gaps, margin)
   local blueprint = util.get_blueprint(player.cursor_stack)
   if not blueprint then return end
 
-  local entities = blueprint.get_blueprint_entities()
+  local entities = blueprint.get_blueprint_entities() or {}
+  local tiles = blueprint.get_blueprint_tiles() or {}
   local entity_prototypes = game.entity_prototypes
 
-  local tiles = {}
+  local output_tiles = {}
   local tile_index = 0
 
+  local first_obj = entities[1] or tiles[1]
+
   if fill_gaps then
-    local TileArea = area.load(area.from_position(entities[1].position))
+    local TileArea = area.load(area.from_position(first_obj.position))
 
     -- iterate entities to calculate needed grid size
     for _, entity in pairs(entities) do
@@ -28,6 +31,15 @@ return function(player, tile_name, fill_gaps, margin)
         TileArea:expand_to_contain_area(area.move(box, entity.position))
       end
     end
+    -- and tiles
+    for _, tile in pairs(tiles) do
+      local box = {
+        left_top = {x = tile.position.x, y = tile.position.y},
+        right_bottom = {x = tile.position.x + 1, y = tile.position.y + 1}
+      }
+      TileArea:expand_to_contain_area(box)
+    end
+
 
     -- ceil to outside edges and add a margin
     TileArea:ceil():expand(margin)
@@ -35,7 +47,7 @@ return function(player, tile_name, fill_gaps, margin)
     -- for the purpose of this function, we don't care about any pre-existing tiles - we will replace all of them
     for position in TileArea:iterate() do
       tile_index = tile_index + 1
-      tiles[tile_index] = {
+      output_tiles[tile_index] = {
         position = position,
         name = tile_name
       }
@@ -57,7 +69,7 @@ return function(player, tile_name, fill_gaps, margin)
 
       if add then
         tile_index = tile_index + 1
-        tiles[tile_index] = {
+        output_tiles[tile_index] = {
           position = position,
           name = tile_name
         }
@@ -84,7 +96,10 @@ return function(player, tile_name, fill_gaps, margin)
         end
       end
     end
+    for _, tile in pairs(tiles) do
+      add_tile(tile.position)
+    end
   end
 
-  blueprint.set_blueprint_tiles(tiles)
+  blueprint.set_blueprint_tiles(output_tiles)
 end
